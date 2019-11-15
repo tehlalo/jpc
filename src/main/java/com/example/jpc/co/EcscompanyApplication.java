c79f5645fcd234255663b5317f9450b6c6ca2574
Raul Andrade <raul.andrade1992@gmail.com>
2019-11-14 22:58:19 -0800

    Initialize

---
+++ src/main/java/com/example/jpc/co/EcscompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpc.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcscompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcscompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpc/co/CoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpc.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
